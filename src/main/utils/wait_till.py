8858b263ba9f051a28634d47d5666cff21e03891
automationhacks <automationhacks@gmail.com>
2025-12-16 22:41:31 +0530

    Added an example test to install, uninstall app and check for installation state.

---
+++ src/main/utils/wait_till.py
@@ -0,0 +1,21 @@
+import time
+
+
+def wait_till(expected, timeout, sleep_for, sleep_factor, func, *args):
+    start_time = time.time()
+    sleep_time = sleep_for
+
+    while time.time() - start_time < timeout:
+        if args and args[0] is not None:
+            result = func(*args)
+        else:
+            result = func()
+
+        if result == expected:
+            return result
+
+        print("Still not expected result: {}".format(result))
+        print("Snoozing for {} seconds".format(sleep_time))
+        time.sleep(sleep_time * sleep_factor)
+
+    return None
---
+++ src/tests/android/uiautomator2/test_app_install_uninstall.py
@@ -0,0 +1,55 @@
+import unittest
+
+from appium import webdriver
+from appium.options.android import UiAutomator2Options
+from appium.webdriver.common.appiumby import AppiumBy
+from selenium.webdriver.support.wait import WebDriverWait
+
+from src.main.utils.wait_till import wait_till
+
+capabilities = {
+    "platformName": "android",
+    "appium:automationName": "uiautomator2",
+    "appium:platformVersion": "16",
+    "appium:deviceName": "emulator-5554",
+    "appium:app": "/Users/gauravsingh/self/android-apidemos/apks/ApiDemos-debug.apk",
+    "appium:appPackage": "io.appium.android.apis",
+    "appium:appActivity": "io.appium.android.apis.ApiDemos"
+}
+
+appium_server_url = 'http://localhost:4723'
+
+
+class AppInstallUninstallTest(unittest.TestCase):
+    def setUp(self) -> None:
+        load_capabilities = UiAutomator2Options().load_capabilities(capabilities)
+        self.driver = webdriver.Remote(appium_server_url, options=load_capabilities)
+
+    def tearDown(self) -> None:
+        if self.driver:
+            self.driver.quit()
+
+    def test_dialog_text_is_correct(self) -> None:
+        wait_till(True, 10, 2, 0.5, self.is_api_demos_installed, None)
+        is_app_installed = self.is_api_demos_installed()
+        self.assertEqual(is_app_installed, True, 'Oh oh, app is not installed?')
+
+        self.driver.remove_app("io.appium.android.apis")
+        wait_till(False, 10, 2, 0.5, self.is_api_demos_installed, None)
+        is_app_installed = self.is_api_demos_installed()
+        self.assertEqual(is_app_installed, False, 'Oh oh, app is installed?')
+
+        self.driver.install_app("/Users/gauravsingh/self/android-apidemos/apks/ApiDemos-debug.apk")
+        wait_till(True, 10, 2, 0.5, self.is_api_demos_installed, None)
+        is_app_installed = self.is_api_demos_installed()
+        self.assertEqual(is_app_installed, True, 'Oh oh, duh app is not installed again?')
+
+        elem = self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("API Demos")')
+        self.assertEqual(elem.is_displayed(), True)
+
+    def is_api_demos_installed(self) -> bool:
+        return self.driver.is_app_installed("io.appium.android.apis")
+
+
+if __name__ == '__main__':
+    unittest.main()
